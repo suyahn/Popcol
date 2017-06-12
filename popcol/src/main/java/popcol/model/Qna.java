fce7099de9bd60ff1fc836d5ce8dd01118c3d315
suyahn <suyahn@naver.com>
2017-06-12 17:48:32 +0900

    configuration mappers 대문자로 수정

--- popcol/src/main/java/popcol/model/Qna.java
+++ popcol/src/main/java/popcol/model/Qna.java
@@ -8,4 +8,49 @@ public class Qna {
 	private String qreplydate;
 	private String cid;
 	private String qdel;
+	
+	public int getQid() {
+		return qid;
+	}
+	public void setQid(int qid) {
+		this.qid = qid;
+	}
+	public String getQsubject() {
+		return qsubject;
+	}
+	public void setQsubject(String qsubject) {
+		this.qsubject = qsubject;
+	}
+	public String getQcontent() {
+		return qcontent;
+	}
+	public void setQcontent(String qcontent) {
+		this.qcontent = qcontent;
+	}
+	public String getQreplycontent() {
+		return qreplycontent;
+	}
+	public void setQreplycontent(String qreplycontent) {
+		this.qreplycontent = qreplycontent;
+	}
+	public String getQreplydate() {
+		return qreplydate;
+	}
+	public void setQreplydate(String qreplydate) {
+		this.qreplydate = qreplydate;
+	}
+	public String getCid() {
+		return cid;
+	}
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+	public String getQdel() {
+		return qdel;
+	}
+	public void setQdel(String qdel) {
+		this.qdel = qdel;
+	}
+	
+	
 }
