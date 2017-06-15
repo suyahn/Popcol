ed6989f62073c3762f4f1ffa9a6cdbaea0111b4c
suyahn <suyahn@naver.com>
2017-06-15 17:24:11 +0900

    [수연] faq list, view 다시 복구함

--- popcol/src/main/java/popcol/dao/faq/FaqDaoImpl.java
+++ popcol/src/main/java/popcol/dao/faq/FaqDaoImpl.java
@@ -22,4 +22,19 @@ public class FaqDaoImpl implements FaqDao {
 	public List<Faq> list(Faq faq) {
 		return sst.selectList("faqns.list", faq);
 	}
+
+    @Override
+    public int getMaxNum() {
+        return sst.selectOne("faqns.getMaxNum");
+    }
+
+    @Override
+    public int insert(Faq faq) {
+        return sst.insert("faqns.insert", faq);
+    }
+
+    @Override
+    public Faq select(int fid) {
+        return sst.selectOne("faqns.select", fid);
+    }
 }
